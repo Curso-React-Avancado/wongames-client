dc77ef974837e43e256052e1c5d5888bdd108b19
bergmannlucas <lucaasberg@gmail.com>
2021-03-22 17:39:56 -0300

    Adicionando seção de popular games

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -1,6 +1,7 @@
 import { gql } from '@apollo/client'
 import { BannerFragment } from 'graphql/fragments/banner'
 import { GameFragment } from 'graphql/fragments/game'
+import { HighlightFragment } from 'graphql/fragments/highlight'
 
 // GET_HOME | QUERY_HOME
 export const QUERY_HOME = gql`
@@ -8,17 +9,15 @@ export const QUERY_HOME = gql`
     banners {
       ...BannerFragment
     }
-
     newGames: games(
-      where: { release_date_lte: "2021-03-22" }
+      where: { release_date_lte: "2021-01-27" }
       sort: "release_date:desc"
       limit: 8
     ) {
       ...GameFragment
     }
-
     upcomingGames: games(
-      where: { release_date_gt: "2021-03-22" }
+      where: { release_date_gt: "2021-01-27" }
       sort: "release_date:asc"
       limit: 8
     ) {
@@ -27,7 +26,37 @@ export const QUERY_HOME = gql`
     freeGames: games(where: { price: 0 }, sort: "release_date:desc", limit: 8) {
       ...GameFragment
     }
+    sections: home {
+      newGames {
+        title
+        highlight {
+          ...HighlightFragment
+        }
+      }
+      popularGames {
+        title
+        highlight {
+          ...HighlightFragment
+        }
+        games(limit: 8) {
+          ...GameFragment
+        }
+      }
+      upcomingGames {
+        title
+        highlight {
+          ...HighlightFragment
+        }
+      }
+      freeGames {
+        title
+        highlight {
+          ...HighlightFragment
+        }
+      }
+    }
   }
   ${BannerFragment}
   ${GameFragment}
+  ${HighlightFragment}
 `
