c6dcd1ec9af6941b9bbe1cd08337b1c6f88a618d
bergmannlucas <lucaasberg@gmail.com>
2021-03-22 17:13:56 -0300

    Criado GameFragment, HighlightFragment e NewGames

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,24 +18,32 @@ export default function Index(props: HomeTemplateProps) {
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
-  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+  const {
+    data: { banners, newGames }
+  } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
 
   return {
     props: {
       revalidate: 10,
-      banners: data.banners.map((banner) => ({
+      banners: banners.map((banner) => ({
         img: `http://localhost:1337${banner.image?.url}`,
         title: banner.title,
         subtitle: banner.subtitle,
-        buttonLabel: banner.button?.label || null,
-        buttonLink: banner.button?.link || null,
+        buttonLabel: banner.button?.label,
+        buttonLink: banner.button?.link,
         ...(banner.ribbon && {
           ribbon: banner.ribbon.text,
           ribbonColor: banner.ribbon.color,
           ribbonSize: banner.ribbon.size
         })
       })),
-      newGames: gamesMock,
+      newGames: newGames.map((game) => ({
+        title: game.name,
+        slug: game.slug,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337${game.cover?.url}`,
+        price: game.price
+      })),
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
       upcomingGames: gamesMock,
